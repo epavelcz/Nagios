2b7dcf0ce7130b84fb8528128b6a79e388bc07c3
epavel <pavel.sajnar@memos.cz>
2019-01-02 14:49:39 +0100

    V1

---
+++ PVE-backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#default valus
+#default value is 1=warning, if something goes wrong in for file clause
+result=""
+errCode=1
+
+for file in `ls /var/log/pve/tasks/[0-9A-F]/*vzdump* -Art | tail -n 1`
+
+do
+        result=`grep 'ERROR' $file|grep VM`
+        if [ "x$result" != "x" ] ; then
+                errCode=2
+        else
+                errCode=0
+                result=`echo $((( $(date +%s) - $(stat -c %Y $file)) / 3600 ))`
+        fi
+
+done
+
+case "$errCode" in
+
+        0)
+                echo "OK - Backup log age:" $result "hours"
+                
+                exit 0
+        ;;
+
+        1)
+                echo "WARNING - Something went wrong with for file. Please check permissions"
+                exit 1
+        ;;
+        2)
+                echo $result
+                exit 2
+        ;;
+
+esac
